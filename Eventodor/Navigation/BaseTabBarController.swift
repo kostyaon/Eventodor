b77817f8b395bc07e67d85b3839d8247d451244d
Konstantin <konstantin.petrikevich@gmail.com>
2021-12-13 18:52:17 +0300

    Initialize project and add tab bar

---
+++ Eventodor/Navigation/BaseTabBarController.swift
@@ -0,0 +1,66 @@
+//
+//  BaseTabBarController.swift
+//  Eventodor
+//
+//  Created by Petrykevich, Kanstantsin on 13.12.21.
+//
+
+import UIKit
+
+class BaseTabBarController: UITabBarController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle method's
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupTabBar()
+        setupVC()
+    }
+}
+
+private
+extension BaseTabBarController {
+    
+    func createNavBar(for rootVC: UIViewController, title: String, image: String) -> UIViewController {
+        let navController = UINavigationController(rootViewController: rootVC)
+        navController.tabBarItem.title = title
+        navController.tabBarItem.image = UIImage(named: image)?.withRenderingMode(.alwaysTemplate)
+        return navController
+    }
+    
+    func setupTabBar() {
+        let tabBar = UITabBar.appearance()
+        tabBar.isTranslucent = false
+        tabBar.barTintColor = .black
+        let color = UIColor.white
+       
+        let fontColor = UIColor(red: 236/256, green: 0.0, blue: 0.0, alpha: 1.0)
+        let unselectedColor = UIColor.black
+        if #available(iOS 15, *) {
+            let tabBarAppearance = UITabBarAppearance()
+            tabBarAppearance.backgroundColor = color
+            tabBarAppearance.stackedLayoutAppearance.selected.titleTextAttributes = [.foregroundColor: fontColor, .font: UIFont.boldSystemFont(ofSize: 13)]
+            tabBarAppearance.stackedLayoutAppearance.normal.titleTextAttributes = [.foregroundColor: unselectedColor, .font: UIFont.systemFont(ofSize: 13)]
+            tabBarAppearance.stackedLayoutAppearance.normal.iconColor = unselectedColor
+            tabBar.standardAppearance = tabBarAppearance
+            tabBar.scrollEdgeAppearance = tabBarAppearance
+        } else {
+            tabBar.barTintColor = color;
+            UITabBarItem.appearance().setTitleTextAttributes([NSAttributedString.Key.font: UIFont.systemFont(ofSize: 13)], for: .normal)
+        }
+        
+        tabBar.tintColor = fontColor
+        tabBar.unselectedItemTintColor = unselectedColor
+    }
+    
+    func setupVC() {
+        viewControllers = [
+            createNavBar(for: UIViewController(), title: "Event", image: "feed_tab_icon"),
+            createNavBar(for: UIViewController(), title: "Map", image: "score_tab_icon"),
+            createNavBar(for: UIViewController(), title: "My Events", image: "logbook_tab_icon"),
+            createNavBar(for: UIViewController(), title: "Profile", image: "bonus_tab_icon")
+        ]
+    }
+}
