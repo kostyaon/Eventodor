b7bfe8f99d59473577bd2bda5628f16851654964
Konstantin <konstantin.petrikevich@gmail.com>
2022-05-25 17:09:32 +0300

    Create a BaseMapViewController

--- Eventodor/Screens/Tabs/EventsTab/CreateEventCard/CreateEventCardViewController.swift
+++ Eventodor/Screens/Tabs/EventsTab/CreateEventCard/CreateEventCardViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class CreateEventCardViewController: BaseViewController {
+class CreateEventCardViewController: BaseMapViewController {
     
     // MARK: - Outlets
     @IBOutlet weak var bgView: UIView!
@@ -23,10 +23,8 @@ class CreateEventCardViewController: BaseViewController {
     @IBOutlet weak var dateTextField: EVENTODORTextField!
     @IBOutlet weak var priceTextField: EVENTODORTextField!
     @IBOutlet weak var createButton: EVENTODORButton!
-    @IBOutlet weak var mapView: MKMapView!
     
     // MARK: - Properties
-    private var locationManager = CLLocationManager()
     private var eventLocationAnnotation = MKPointAnnotation()
     private var isEventLocationSet = false
     
@@ -36,6 +34,7 @@ class CreateEventCardViewController: BaseViewController {
         
         dismissKeyboardOnTap = true
         setupUI()
+        configureMap()
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -75,14 +74,24 @@ extension CreateEventCardViewController {
         setupCardView()
         setupAction()
         setupText()
-        setupMapView()
-        defineMappingRegion()
+        setupGestures()
+    }
+    
+    func configureMap() {
+        mapView.delegate = self
+        locationManager.delegate = self
+    }
+    
+    func setupGestures() {
+        let longTapGesture = UILongPressGestureRecognizer(target: self, action: #selector(onLongPressure))
+        mapView.addGestureRecognizer(longTapGesture)
     }
     
     func setupText() {
         createButton.setTitle(with: "create_event_title".localized())
         createButton.onTap = { [weak self] in
-            print(self?.eventLocationAnnotation.coordinate)
+            guard let this = self else { return }
+            print(this.eventLocationAnnotation.coordinate)
         }
         titleLabel.text = "create_event_button".localized()
     }
@@ -97,78 +106,5 @@ extension CreateEventCardViewController {
         cardView.layer.shadowOpacity = 0.5
         cardView.layer.shadowOffset = CGSize(width: 0.0, height: 10.0)
         cardView.layer.shadowRadius = 15
-    }
-    
-    func setupMapView() {
-        mapView.delegate = self
-        let initialLocation = CLLocation(latitude: 53.91974976740907, longitude: 27.593096852856355)
-        mapView.centerToLocation(initialLocation)
-        
-        let longTapGesture = UILongPressGestureRecognizer(target: self, action: #selector(onLongPressure))
-        mapView.addGestureRecognizer(longTapGesture)
-    }
-    
-    func defineMappingRegion() {
-        let minskCenter = CLLocation(latitude: 53.91974976740907, longitude: 27.593096852856355)
-        let region = MKCoordinateRegion(
-            center: minskCenter.coordinate,
-            latitudinalMeters: 20000,
-            longitudinalMeters: 25000)
-        mapView.setCameraBoundary(
-            MKMapView.CameraBoundary(coordinateRegion: region),
-            animated: true)
-        let zoomRange = MKMapView.CameraZoomRange(maxCenterCoordinateDistance: 50000)
-        mapView.setCameraZoomRange(zoomRange, animated: true)
-    }
-    
-    func getCurrentLocation() {
-        locationManager = CLLocationManager()
-        locationManager.delegate = self
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        locationManager.requestAlwaysAuthorization()
-        if CLLocationManager.locationServicesEnabled() {
-            locationManager.startUpdatingLocation()
-        }
-    }
-}
-
-// MARK: - MKMapViewDelegate method's
-extension CreateEventCardViewController: MKMapViewDelegate {
-    
-    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-        if annotation.title == "your_location".localized() {
-            let view = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: "MyAnnotation")
-            view.markerTintColor = .green
-            view.isDraggable = false
-            return view
-        } else {
-            let eventMark = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: "MyAnnotation")
-            eventMark.markerTintColor = .red
-            eventMark.isDraggable = true
-            return eventMark
-        }
-    }
-}
-
-
-// MARK: - CLLocationManagerDelegate method's
-extension CreateEventCardViewController: CLLocationManagerDelegate {
-    
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        let currentLocation:CLLocation = locations[0] as CLLocation
-        let center = CLLocationCoordinate2D(latitude: currentLocation.coordinate.latitude, longitude: currentLocation.coordinate.longitude)
-        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
-        mapView.setRegion(region, animated: true)
-        
-        // Indicate user's location
-        let myLocation = MKPointAnnotation()
-        myLocation.coordinate = CLLocationCoordinate2DMake(currentLocation.coordinate.latitude, currentLocation.coordinate.longitude)
-        myLocation.title = "your_location".localized()
-        mapView.addAnnotation(myLocation)
-    }
-    
-    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        showError(title: "error_title".localized(), message: "location_error".localized())
-        print("Error - locationManager: \(error.localizedDescription)")
     }
 }
